ad9cdd768745e85eb2621220165506dce67a717e
ankitn-navsoft <ankit@navsoft.in>
2022-07-21 00:19:02 +0530

    api change

--- src/Services/UrlApi.js
+++ src/Services/UrlApi.js
@@ -1,6 +1,4 @@
-// export const CurrentUrl =
-//   !process.env.NODE_ENV || process.env.NODE_ENV === "development"
-//     ? "https://localhost:44376/"
-//     : "http://lp.ivikas.in/";
-
-export const CurrentUrl = "http://lp.ivikas.in/";
+export const CurrentUrl =
+  !process.env.NODE_ENV || process.env.NODE_ENV === "development"
+    ? "https://localhost:44376/"
+    : "http://pan.gipinfosystems.com/";
